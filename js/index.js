7b601873acdcdae30ffb423a580e8c5b17f066d4
冯哲奇 <Tommy@fengzheqideMacBook-Pro.local>
2016-05-02 00:27:43 +0800

    finish the unslider

--- js/index.js
+++ js/index.js
@@ -20,6 +20,8 @@
             userTab = (navigator.userAgent.indexOf('Android') > 0 && navigator.userAgent.indexOf('Mobile') == -1) || navigator.userAgent.indexOf('iPad') > 0,
             userPhone = (navigator.userAgent.indexOf('iPhone') > 0 && navigator.userAgent.indexOf('iPad') == -1) || navigator.userAgent.indexOf('iPod') > 0 || (navigator.userAgent.indexOf('Android') > 0 && navigator.userAgent.indexOf('Mobile') > 0);
 
+
+
         /*
          RECRUIT modal
          */
